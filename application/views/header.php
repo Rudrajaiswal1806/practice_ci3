babc3c1c5544b9387c20296eb43f8217b07ef6bb
Rudrajaiswal1806 <70509031+Rudrajaiswal1806@users.noreply.github.com>
2021-09-24 09:15:36 +0530

    header update

--- application/views/header.php
+++ application/views/header.php
@@ -5,9 +5,11 @@
  </head>
  <body>
  	<nav>
+	 	<?php echo anchor('/', 'Home') ?>
  		<?php echo anchor('pages/view/about', 'About') ?>
  		<?php echo anchor('pages/view/service', 'Service') ?>
  		<?php echo anchor('pages/view/contact', 'Contact') ?>
+		 <?php echo anchor('users/signup', 'Sign Up') ?>
  	</nav>
  
  
